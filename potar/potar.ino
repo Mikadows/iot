d1fc407727c6beb4a5185609ef9b754418ebc172
Gwendal <gwendou95@hotmail.fr>
2022-06-09 16:59:16 +0200

    Green light/Red light

--- potar/potar.ino
+++ potar/potar.ino
@@ -1,4 +1,6 @@
-#define RED_LED 18
+#define RED_LED_PIN 18
+#define GREEN_LED_PIN 19
+#define POTAR_PIN 15
 
 float floatMap(float x, float in_min, float in_max, float out_min, float out_max) {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
@@ -9,13 +11,14 @@ void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
 
-  pinMode(RED_LED, OUTPUT);
+  pinMode(RED_LED_PIN, OUTPUT);
+  pinMode(GREEN_LED_PIN, OUTPUT);
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
   // read the input on analog pin GIOP36:
-  int analogValue = analogRead(15);
+  int analogValue = analogRead(POTAR_PIN);
   // Rescale to potentiometer's voltage (from 0V to 3.3V):
   float voltage = floatMap(analogValue, 0, 4095, 0, 3.3);
 
@@ -27,7 +30,7 @@ void loop() {
 
   isRoadClosed(analogValue);
   
-  delay(100);
+  delay(1000);
 
   Serial.println("---------------------------");
 }
@@ -49,11 +52,13 @@ void isRoadClosed(int value) {
 
   if (value <= 1200) {
     // Turn local led ON
-    digitalWrite(18, HIGH);
+    digitalWrite(RED_LED_PIN, HIGH);
+    digitalWrite(GREEN_LED_PIN, LOW);
     Serial.println("ROAD CLOSED => Local LED ON");
   } else {
     // Turn local led OFF
-    digitalWrite(18, LOW);
+    digitalWrite(RED_LED_PIN, LOW);
+    digitalWrite(GREEN_LED_PIN, HIGH);
     Serial.println("ROAD OPEN => Local LED OFF");
   }
 }
