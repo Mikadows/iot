0ce73a986ef5b4852018f8ddbe89f7f6f3eade5e
Marion MOTA <katyanne_mm@live.fr>
2022-06-10 16:34:35 +0200

    Button all good

--- button/button/button.ino
+++ button/button/button.ino
@@ -2,7 +2,7 @@
 #include <PubSubClient.h> //Librairie pour la gestion Mqtt 
 
 #define BUZZER_PIN  18
-#define LED 14
+#define LED 19
 #define PUSH_BUTTON 32
 
 int lastState = HIGH;
@@ -11,21 +11,12 @@ const char* ssid = "ESGI";
 const char* password = "Reseau-GES";
 //MQTT
 const char* mqtt_server = "test.mosquitto.org";//Adresse IP du Broker Mqtt
-const int mqttPort = 1883; //port utilisé par le Broker 
 char gateState = '0';
+long tps = 0;
 
 
 WiFiClient espClient;
 PubSubClient client(espClient);
-// the setup function runs once when you press reset or power the board
-void setup() {
-  Serial.begin(9600);
-  pinMode(LED, OUTPUT);
-  pinMode(PUSH_BUTTON, INPUT_PULLUP);
-  pinMode(BUZZER_PIN, OUTPUT); 
-  setup_wifi();
-  setup_mqtt();
-}
 
 void setup_wifi(){
   //connexion au wifi
@@ -42,9 +33,8 @@ void setup_wifi(){
   Serial.println(WiFi.localIP());
 }
 void setup_mqtt(){
-  client.setServer(mqtt_server, mqttPort);
+  client.setServer(mqtt_server, 1883);
   client.setCallback(callback);//Déclaration de la fonction de souscription
-  reconnect();
 }
 
 void callback(char* topic, byte *payload, unsigned int length) {
@@ -52,21 +42,10 @@ void callback(char* topic, byte *payload, unsigned int length) {
    Serial.print("Canal:");
    Serial.println(topic);
    Serial.print("donnee:");
-   Serial.write(payload, length);
+   //Serial.write(payload, length);
    gateState = (char)payload[0];
+   Serial.println(gateState);
  }
-
-void reconnect(){
-  while (!client.connected()) {
-    if (!client.connect("ESP32Client")) {
-      Serial.print("echec, code erreur= ");
-      Serial.println(client.state());
-      Serial.println("nouvel essai dans 2s");
-      delay(1000);
-    }
-  }
-  client.subscribe("iot-projo-gate-angle");
-}
 
 void blinkLed(){
   if(stateLed==HIGH){
@@ -79,39 +58,74 @@ void blinkLed(){
 
 void blinkBuzzerError(){
   if(stateLed==HIGH && gateState == '9'){
-    digitalWrite(LED, HIGH);
     digitalWrite(BUZZER_PIN, HIGH);
+    digitalWrite(LED, HIGH);
     delay(200);
-    digitalWrite(LED, LOW);
     digitalWrite(BUZZER_PIN, LOW);
+    digitalWrite(LED, LOW);
     delay(200);
   }
 }
 
+void publish(char* msg_out){
+    client.publish("dam/presence", msg_out);
+//    delay(100);
+}
+
+void to_subscribe(){
+  client.loop();
+  if (millis()-tps>10000){
+   tps=millis();
+   float temp = random(30);
+ }
+}
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  Serial.begin(9600);
+  pinMode(LED, OUTPUT);
+  pinMode(PUSH_BUTTON, INPUT_PULLUP);
+  pinMode(BUZZER_PIN, OUTPUT); 
+  setup_wifi();
+  setup_mqtt();
+}
 
 // the loop function runs over and over again forever
-void loop() { 
+void loop() {
+
+  if (!client.connected())
+  {
+    if (client.connect("ESP32_clientID")) {
+      client.subscribe("iot-projo-gate-angle");
+    }
+    else {
+      Serial.print("echec, code erreur= ");
+      Serial.println(client.state());
+      Serial.println("nouvel essai dans 1s");
+    }
+  }
+  
   int currentState = digitalRead(PUSH_BUTTON);
   //print out the value of the pushbutton
   if (lastState == LOW && currentState == HIGH){
     lastState = HIGH;
     if(stateLed == LOW){
       stateLed = HIGH;
-      digitalWrite(LED, stateLed);
     }else{
       stateLed = LOW;
       digitalWrite(LED, stateLed);
     }
+    blinkLed();
     Serial.print("Publish message: ");
     Serial.println(stateLed);
     char msg_out[20];
     sprintf(msg_out, "%d", stateLed);
-    reconnect();
-    client.publish("dam/presence", msg_out);
+    publish(msg_out);
   }
   blinkLed();
   blinkBuzzerError();
   lastState = currentState;
-  reconnect();
+
   client.loop();
+  delay(50);
 }
